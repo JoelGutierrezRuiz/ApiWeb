17eaf850cc7ec436dc0ca9eeb59567eb4dc9dc7f
joel gutierrez ruiz <joelgutierrezruiz11@gmail.com>
2023-01-29 18:33:20 +0100

    Estamos implementando las visitas a la web

--- Server/main.js
+++ Server/main.js
@@ -1,6 +1,8 @@
 const Express = require("express")
 var cors = require("cors")
 const morgan = require("morgan")
+require("./database")
+const Task =require("./task.js")
 /*utilizamos las variables*/
 
 
@@ -19,6 +21,14 @@ app.listen(5000,()=>{
 
 
 
+app.get("/datos",cors(),(req,res)=>{
+    res.send("alguien ha entrado a la web")
+    console.log("Hemos recibido una peticion nueva")
+})
+
 app.get("/:pagina",cors(),(req,res)=>{
+    const pagina = req.params.pagina
     console.log(req.params.pagina)
+    const task = new Task(pagina)
+    task.save(task).then(()=>{console.log("Hemos guardado esta pagina")})
 5})
